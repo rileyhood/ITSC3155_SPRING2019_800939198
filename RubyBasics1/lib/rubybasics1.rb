c13d7f37832b9ef86cd67f985460c7e41ede8207
Riley Hood <rhood6@uncc.edu>
2019-09-03 14:36:48 +0000

    RubyBasics1 - Part I

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,19 @@
+# Lab 1
+# Part I
+def sum arr
+  sum=0
+  for x in arr
+  sum=sum+x
+end
+sum
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
