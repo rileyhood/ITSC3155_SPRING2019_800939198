77ac2ea99e1e44b48a60d75316c4300644777e3d
Riley Hood <rhood6@uncc.edu>
2019-09-23 00:36:14 +0000

    Lab 10 initial commit

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -2,15 +2,36 @@
 
 # Part I
 def hello(name)
-  # YOUR CODE HERE
+ name = "Hello, #{name}"
+ name
 end
 
 # Part II
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  a = false
+  if s[0].nil?
+    a=false
+  elsif s[0].match(/[b-dB-Df-hF-Hj-nJ-Np-tP-Tv-zV-Z]/)
+    a=true
+  else
+    a=false
+  end
+  a
 end
 
 # Part III
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  a = false
+  if s[0].nil?
+    a=false
+  elsif s.match(/[^10]/)
+    a= false
+  elsif s.match(/[1]/) && s.match(/[0]/)
+    if s[-1] == "0" && s[-2] == "0"
+      a = true
+    else
+      a = false
+    end
+  end
+  a
 end
