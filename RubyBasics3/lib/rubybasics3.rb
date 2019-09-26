87ccdd90a4f81d97d4162a6d7a302b878367ec25
Riley Hood <rhood6@uncc.edu>
2019-09-26 14:23:26 +0000

    Initial commit for Rails Basics 1

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,18 @@
 # Ruby Basics Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    attr_accessor :isbn, :price
+    def initialize isbn, price
+        @isbn = isbn
+        @price = price
+    begin
+        raise ArgumentError.new("Empty string") if @isbn == ""
+      raise ArgumentError.new("Price is less than or equal to zero") if @price <= 0
+    end
+    end
+    
+    def price_as_string
+        @price = "$#{'%.2f' % @price}"
+    end
+    
 end
