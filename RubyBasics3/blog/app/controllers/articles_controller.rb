0b408cee85a1c5c10a57101b917835a6ac6b8cd9
Riley Hood <rhood6@uncc.edu>
2019-10-13 18:50:38 +0000

    Complete Create and Show.

--- RubyBasics3/blog/app/controllers/articles_controller.rb
+++ RubyBasics3/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,19 @@
 class ArticlesController < 
+    def show
+        @article = Article.find(params[:id])
+    end
     def new
     end
+    
+    def create
+        @article = Article.new(article_params)
+        
+        @article.save
+        redirect_to @article
+    end 
 end
+
+private 
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
